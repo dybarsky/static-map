efb8a0f8b8644c85b03f480c1e6ad17e1743e830
Maxim Dybarsky <maxim.dybarskyy@gmail.com>
2014-07-28 16:54:52 +0300

    Add static-map-generator project skeleton added

---
+++ library/src/main/java/d_max/static_map/RequestBuilder.java
@@ -0,0 +1,11 @@
+package d_max.static_map;
+
+/**
+ * @user: Maxim Dybarsky | maxim.dybarskyy@gmail.com
+ * @date: 7/28/14
+ * @time: 4:59 PM
+ */
+public interface RequestBuilder {
+
+
+}
---
+++ library/src/main/java/d_max/static_map/StaticMap.java
@@ -0,0 +1,9 @@
+package d_max.static_map;
+
+/**
+ * @user: Maxim Dybarsky | mdy@ciklum.com
+ * @date: 7/28/14
+ * @time: 4:59 PM
+ */
+public class StaticMap {
+}
