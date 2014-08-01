77ac54fc8e81f2f72ccf825e7d78dd1e28653263
Maxim Dybarsky <maxim.dybarskyy@gmail.com>
2014-08-01 12:17:52 +0300

    Use default values in smple app

--- sample/src/main/java/d_max/static_map/sample/MapsAdapter.java
+++ sample/src/main/java/d_max/static_map/sample/MapsAdapter.java
@@ -28,22 +28,23 @@ import static android.widget.AbsListView.LayoutParams.MATCH_PARENT;
  */
 public class MapsAdapter extends BaseAdapter {
 
+    private static final float DEFAULT_LATITUDE = 50.4020355f;
+    private static final float DEFAULT_LONGITUDE = 30.5326905f;
     private static final int SIZE = 400; // px
     private static final int MAP_DEFAULT = 0;
     private static final int MAP_SECURE = 1;
     private static final int MAP_ZOOM = 2;
     private static final int MAP_SIZE = 3;
-    private static final int MAP_SCALE= 4;
+    private static final int MAP_SCALE = 4;
     private static final int MAP_SATELLITE = 5;
     private static final int MAP_HYBRID = 6;
     private static final int MAP_TERRAIN = 7;
     private static final int MAP_ADDRESS = 8;
     private static final int MAP_LOCATION = 9;
-    private static final int MAP_MARKER_DEFAULT = 10;
-    private static final int MAP_MARKER_ADDRESS = 11;
-    private static final int MAP_MARKER_LOCATION = 12;
-    private static final int MAP_MARKER_STYLE = 13;
-    private static final int COUNT = 14;
+    private static final int MAP_MARKER_ADDRESS = 10;
+    private static final int MAP_MARKER_LOCATION = 11;
+    private static final int MAP_MARKER_STYLE = 12;
+    private static final int COUNT = 13;
 
     private Context context;
     private Bitmap placeHolder;
@@ -98,75 +99,92 @@ public class MapsAdapter extends BaseAdapter {
         return view;
     }
 
+    private Config createDefaultConfig() {
+        Config config = new Config();
+        config.setImageSize(SIZE - 100, SIZE);
+        config.setCenter(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+        config.setZoom(5);
+        return config;
+    }
+
     private void loadMap(final int position, final ImageView view) {
         Marker marker;
-        Config config = new Config();
+        Config config;
         switch (position) {
             case MAP_DEFAULT:
-                /* change nothing */
+                config = createDefaultConfig();
                 break;
 
             case MAP_SECURE:
+                config = createDefaultConfig();
                 config.setSecure(true);
                 break;
 
             case MAP_ZOOM:
+                config = createDefaultConfig();
                 config.setZoom(10);
                 break;
 
             case MAP_SIZE:
+                config = createDefaultConfig();
                 config.setImageSize(200, 200);
                 break;
 
             case MAP_SCALE:
+                config = createDefaultConfig();
                 config.setImageSize(200, 200);
                 config.setScale(2);
                 break;
 
             case MAP_SATELLITE:
+                config = createDefaultConfig();
                 config.setMapType(Config.MapType.satellite);
                 break;
 
             case MAP_HYBRID:
+                config = createDefaultConfig();
                 config.setMapType(Config.MapType.hybrid);
                 break;
 
             case MAP_TERRAIN:
+                config = createDefaultConfig();
                 config.setMapType(Config.MapType.terrain);
                 break;
 
             case MAP_ADDRESS:
+                config = createDefaultConfig();
                 config.setAddress("Ukraine");
                 break;
 
             case MAP_LOCATION:
-                config.setCenter(50, 0);
-                break;
-
-            case MAP_MARKER_DEFAULT:
-                config.addMarker();
+                config = createDefaultConfig();
+                config.setCenter(50, 30);
                 break;
 
             case MAP_MARKER_ADDRESS:
+                config = createDefaultConfig();
                 marker = config.addMarker();
                 marker.setAddress("Kyiv");
                 break;
 
             case MAP_MARKER_LOCATION:
+                config = createDefaultConfig();
                 marker = config.addMarker();
-                marker.setLocation(50, 2);
+                marker.setLocation(50, 32);
                 config.setZoom(5);
                 break;
 
             case MAP_MARKER_STYLE:
+                config = createDefaultConfig();
                 marker = config.addMarker();
+                marker.setLocation(50, 28);
                 marker.setLabel("M");
                 marker.setColor("0x00FFFF");
                 marker.setSize(Marker.Size.mid);
-                config.setZoom(14);
+                config.setZoom(7);
                 break;
 
-            default:
+            default: config = null;
         }
 
         Callback callback = new Callback() {
